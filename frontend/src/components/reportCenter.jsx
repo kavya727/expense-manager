4a92740553197f98f61e8460970239a2e1c8b2e5
Kavya Somasundar <kavsomas@in.ibm.com@Kavyas-MacBook-Pro.local>
2020-05-01 16:50:53 -0400

    monthly report view with cosmetics

--- frontend/src/components/reportCenter.jsx
+++ frontend/src/components/reportCenter.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { select, csv, scaleLinear, max, scaleBand } from "d3";
-import orderBy from "lodash/orderBy";
-import { getExpenses } from "../services/expensesService";
+import { Link } from "react-router-dom";
+import { select, scaleLinear, max, scaleBand } from "d3";
 
 class Reports extends Component {
   state = {};
@@ -16,35 +15,57 @@ class Reports extends Component {
   getGrapgh = data => {
     const svg = select("svg");
     if (svg && data) {
-      const width = "960"; // +svg.attr("width");
-      const height = "50"; // +svg.attr("height");
+      const width = "50"; // +svg.attr("width");
+      const height = "600"; // +svg.attr("height");
       data.forEach((item, index) => {
-        item.index = index * height;
+        item.index = index * 80;
       });
 
       console.log("data", data);
       const xScale = scaleLinear()
         .domain([0, max(data, d => d.amount)])
-        .range([0, width]);
+        .range([0, height - 50]);
 
       const yScale = scaleBand()
         .domain([data.map(d => d.category)])
-        .range([0, height]);
-
-      console.log(yScale.range());
+        .range([0, width]);
 
       svg
         .selectAll("rect")
         .data(data)
         .enter()
         .append("rect")
-        .attr("width", d => xScale(d.amount))
-        .attr("height", yScale.bandwidth())
-        .attr("y", d => d.index)
+        .attr("width", yScale.bandwidth())
+        .attr("height", d => xScale(d.amount))
+        // .attr("width", d => xScale(d.amount))
+        // .attr("height", yScale.bandwidth())
+        .attr("x", d => d.index)
+        .attr("y", d => height - xScale(d.amount))
         .attr("fill", "#8c8787")
+        .attr("stroke", "black")
         .attr("className", "rect-class");
 
-      console.log("y", yScale("Housing"));
+      svg
+        .selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("x", d => d.index)
+        .attr("y", d => height - xScale(d.amount) - 5)
+        .text(d => d.amount)
+        .attr("font-size", "medium")
+        .attr("fill", "#8c8787");
+      svg
+        .selectAll("label")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("x", d => d.index)
+        .attr("y", d => height - xScale(d.amount) - 20)
+        .text(d => d.category)
+        .attr("fill", "#8c8787")
+        .attr("display", "flex")
+        .attr("font-size", "medium");
     }
   };
 
@@ -52,8 +73,17 @@ class Reports extends Component {
     return (
       <>
         <div className="reportContainer">
-          <h1 className="header-text">Report Center</h1>
-          <svg width="960" height="500" className="svg-class"></svg>
+          <h1 className="header-text">Monthly Report</h1>
+          <Link
+            className="gobackLink"
+            to={{
+              pathname: "/expenses"
+            }}
+          >
+            Go Back
+          </Link>
+          <span className="headertext-border"></span>
+          <svg width="960" height="1000" className="svg-class"></svg>
           {this.state.activeExpense
             ? this.getGrapgh(this.state.activeExpense.items)
             : null}
